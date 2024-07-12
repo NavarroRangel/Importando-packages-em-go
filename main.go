a595e0b9076bcf8762b6745a160bce688ae0b740
Navarro rangel de souza <navarrorangel1@hotmail.com>
2024-07-12 18:24:39 -0300

    Separando novas funcionalidades

--- main.go
+++ main.go
@@ -3,23 +3,13 @@ package main
 import (
 	"fmt"
 	"example.com/meuprojeto/contas"
+	//"example.com/meuprojeto/clientes"
 )
 
 
 
 func main() {
-	contaNavarro := contas.ContaCorrente {Titular: "navarro", Saldo: 300}
-	contaJoao := contas.ContaCorrente {Titular: "joao", Saldo: 500}
-
-	status := contaNavarro.Transferir(-200,&contaJoao)
-	
-	
-	fmt.Println(status)
-	fmt.Println(contaNavarro)
-	fmt.Println(contaJoao)
-
-	;
-
-
-	
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
+	fmt.Println(contaExemplo.ObterSaldo())
 }
